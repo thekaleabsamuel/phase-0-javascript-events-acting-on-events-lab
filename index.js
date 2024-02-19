2cfb2b5ab148cda38a8b6afeeeeee9a1041963f1
Kaleab Samuel <thekaleabsamuel@gmail.com>
2024-02-18 21:51:57 -0700

    adds myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+// Get the dodger element
+const dodger = document.getElementById('dodger');
+
+// moveDodgerLeft function
+function moveDodgerLeft() {
+  const leftPosition = parseInt(dodger.style.left);
+  if (leftPosition > 0) {
+    dodger.style.left = `${leftPosition - 4}px`;
+  }
+}
+
+// moveDodgerRight function
+function moveDodgerRight() {
+  const leftPosition = parseInt(dodger.style.left);
+  if (leftPosition < 360) {
+    dodger.style.left = `${leftPosition + 4}px`;
+  }
+}
+
+// Export functions if using Node.js/CommonJS
+// module.exports = { moveDodgerLeft, moveDodgerRight };
+
+// Export functions if using ES6 modules
+// export { moveDodgerLeft, moveDodgerRight };
